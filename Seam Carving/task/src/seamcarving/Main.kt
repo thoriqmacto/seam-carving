2559728597787785387c2e18f8ebb73cb2e4ebc2
thoriqmacto <thariq.hadad@gmail.com>
2023-12-16 12:18:26 +0700

    Finish stage-3

--- Seam Carving/task/src/seamcarving/Main.kt
+++ Seam Carving/task/src/seamcarving/Main.kt
@@ -12,15 +12,26 @@ fun main(args:Array<String>) {
     var importFile:String? = null
     var exportFile:String? = null
 
+    val listFiles = mutableListOf<String>()
+
     args.forEachIndexed{ index,arg ->
         when(arg){
-            "-in"  -> importFile = args.getOrNull(index+1)
-            "-out" -> exportFile = args.getOrNull(index+1)
+            "-in"       -> importFile = args.getOrNull(index+1)
+            "-out"      -> exportFile = args.getOrNull(index+1)
+            "-compare"  -> {
+                for (i in args.indices){
+                    if (i>0) {
+                        val file = args.getOrNull(i)
+                        listFiles.add(file?:"")
+                    }
+                }
+            }
         }
     }
 
     val carver = SeamCarving()
     carver.produce(importFile,exportFile)
+    //carver.compare(listFiles)
 
 }
 
@@ -49,14 +60,19 @@ class SeamCarving{
             val image = ImageIO.read(File(folderPath+importFile))
             val energy = calculateEnergy(image)
             val maxEnergy = getMaxEnergy(energy)
-            val normEnergy = normalizeEnergies(energy,maxEnergy)
+            // println(maxEnergy)
+            val listToFile = mutableListOf<String>()
 
             for (x in 0 until image.width){
                 for (y in 0 until image.height){
-                    val colorNew = Color(normEnergy[x][y],normEnergy[x][y],normEnergy[x][y])
+                    val normEnergy = (255.0 * energy[x][y] / maxEnergy).toInt()
+                    val colorNew = Color(normEnergy,normEnergy,normEnergy)
                     image.setRGB(x,y,colorNew.rgb)
+
+                    // listToFile.add("$x,$y: ${energy[x][y]} | $normEnergy")
                 }
             }
+            // saveToTextFile(listToFile,"${folderPath}produceTest.txt")
 
             // write to file
             ImageIO.write(image, "png", File(folderPath+exportFile))
@@ -72,22 +88,11 @@ class SeamCarving{
             for (value in row){
                 if (value > max){
                     max = value
+                    // println("$row,$value: $max")
                 }
             }
         }
         return max
-    }
-
-    private fun normalizeEnergies(energy:Array<DoubleArray>,maxEnergyValue:Double):Array<IntArray>{
-        val output = Array(energy.size) { IntArray(energy[0].size) }
-
-        for (x in energy.indices){
-            for (y in energy[0].indices){
-                output[x][y] = (255.0 * energy[x][y] / maxEnergyValue).toInt()
-            }
-        }
-
-        return output
     }
 
     private fun calculateEnergy(image: BufferedImage):Array<DoubleArray> {
@@ -99,15 +104,30 @@ class SeamCarving{
             for(y in 0 until height){
                 val gx = computeGradientX(image, x, y, width)
                 val gy = computeGradientY(image, x, y, height)
-                energy[x][y] = sqrt( gx.pow(2.0) + gy.pow(2.0))
+                energy[x][y] = sqrt((gx + gy).toDouble())
             }
         }
         return energy
     }
 
-    private fun computeGradientX(image: BufferedImage, x: Int, y: Int, width: Int): Double {
-        val leftX = if(x - 1 >= 0) x-1 else x
-        val rightX = if(x + 1 < width) x+1 else x
+    private fun computeGradientX(image: BufferedImage, x: Int, y: Int, width: Int): Int {
+        val leftX = if(x + 1 >= width){
+            x - 2
+        } else if(x - 1 >= 0) {
+            x - 1
+        } else {
+            x
+        }
+
+        val rightX = if(x - 1 < 0){
+            x + 2
+        } else if (x + 1 < width) {
+            x + 1
+        } else {
+            x
+        }
+
+        //print("x->$x: leftX->$leftX, rightX->$rightX | ")
 
         val colorLeft = Color(image.getRGB(leftX,y))
         val colorRight = Color(image.getRGB(rightX,y))
@@ -116,12 +136,27 @@ class SeamCarving{
         val gx = colorLeft.green - colorRight.green
         val bx = colorLeft.blue - colorRight.blue
 
-        return rx.toDouble().pow(2.0) + gx.toDouble().pow(2.0) + bx.toDouble().pow(2.0)
+        return rx * rx + gx * gx + bx * bx
     }
 
-    private fun computeGradientY(image: BufferedImage, x: Int, y: Int, height: Int): Double {
-        val topY = if(y - 1 >= 0) y-1 else y
-        val bottomY = if(y + 1 < height) y+1 else y
+    private fun computeGradientY(image: BufferedImage, x: Int, y: Int, height: Int): Int {
+        val topY = if(y + 1 >= height){
+            y - 2
+        }else if(y - 1 >= 0) {
+            y - 1
+        } else {
+            y
+        }
+
+        val bottomY = if(y - 1 < 0){
+            y + 2
+        } else if(y + 1 < height) {
+            y + 1
+        } else {
+            y
+        }
+
+        //print("y->$y: topY->$topY, bottomY->$bottomY\n")
 
         val colorTop = Color(image.getRGB(x,topY))
         val colorBottom = Color(image.getRGB(x,bottomY))
@@ -130,7 +165,7 @@ class SeamCarving{
         val gy = colorTop.green - colorBottom.green
         val by = colorTop.blue - colorBottom.blue
 
-        return ry.toDouble().pow(2.0)+gy.toDouble().pow(2.0)+by.toDouble().pow(2.0)
+        return ry * ry + gy * gy + by * by
     }
 
     private fun makeNegative(importFile: String?): BufferedImage? {
@@ -144,5 +179,52 @@ class SeamCarving{
             }
         }
         return image
+    }
+
+    private fun getRGBValues(image: BufferedImage): List<String> {
+        val rgbValues = mutableListOf<String>()
+
+        val width = image.width
+        val height = image.height
+
+        for (y in 0 until height) {
+            for (x in 0 until width) {
+                val color = Color(image.getRGB(x, y))
+                val red = color.red
+                val green = color.green
+                val blue = color.blue
+
+                val rgbString = "[$x,$y]: $red,$green,$blue"
+                rgbValues.add(rgbString)
+            }
+        }
+
+        return rgbValues
+    }
+
+    private fun saveToTextFile(rgbValues: List<String>, filePath: String) {
+        val file = File(filePath)
+        file.bufferedWriter().use { writer ->
+            for (rgb in rgbValues) {
+                writer.write("$rgb\n")
+            }
+        }
+        println(filePath)
+    }
+
+    fun compare(compareFiles: MutableList<String>) {
+        try {
+            var i = 1
+            compareFiles.forEach {
+                val image = ImageIO.read(File(folderPath+it))
+                val arrColor = getRGBValues(image)
+                val fileOutputStr = "${folderPath}test_${i}.txt"
+                saveToTextFile(arrColor,fileOutputStr)
+                i++
+            }
+        }catch (e:Exception){
+            val msg = "An error occurred: ${e.message}"
+            println(msg)
+        }
     }
 }
